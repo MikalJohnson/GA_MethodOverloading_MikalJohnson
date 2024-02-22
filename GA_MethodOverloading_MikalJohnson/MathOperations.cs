e3ebda2d69d5403a15e8cfbbf70367f4f9eccee1
MikalJohnson <Johnsonmikal24@gmail.com>
2024-02-21 17:09:29 -0800

    Add project files.

---
+++ GA_MethodOverloading_MikalJohnson/MathOperations.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_MethodOverloading_MikalJohnson
+{
+    internal class MathOperations
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Add(double a, double b, double c)
+        {
+            return a + b + c;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+        public double Subtract(double a, double b, double c)
+        {
+            return a - b - c;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Multiply(double a, double b, double c)
+        {
+            return a * b * c;
+        }
+
+        public int Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentException("Divisor cannot be zero.", nameof(b));
+            }
+            return a / b;
+        }
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentException("Divisor cannot be zero.", nameof(b));
+            }
+            return a / b;
+        }
+    }
+}
